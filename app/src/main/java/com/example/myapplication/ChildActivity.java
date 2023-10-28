78526bb1519672b484ff3d6e53b577c4c286053e
09.02.07 <il.ruzanovil@yandex.ru>
2023-10-28 12:22:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/ChildActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class ChildActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        Button backButton;
+
+
+        backButton = findViewById(R.id.button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // Действия при нажатии кнопки возврата
+            }
+        });
+
+
+
+            }
+
+    }
---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listview);
+
+
+        List<String> list = new ArrayList<>();
+        list.add("Заметка 1");
+        list.add("Заметка 2");
+        list.add("Заметка 3");
+
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,list);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                startActivity(new Intent(MainActivity.this, ChildActivity.class));
+
+
+
+                    }
+                });
+            }
+        }
